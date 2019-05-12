290c48ca04f93dab46233907a498bc6527fa55e7
katherine <katherine.morales017@gmail.com>
2019-05-11 21:57:16 -0500

    PrimerEntregable

---
+++ app/src/main/java/com/example/primerentregable/Listar.java
@@ -0,0 +1,17 @@
+package com.example.primerentregable;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Listar extends AppCompatActivity{
+
+
+}
+
+
+
+
---
+++ app/src/main/java/com/example/primerentregable/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.primerentregable;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnSiguiente;
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnSiguiente = (Button)findViewById(R.id.btnSiguiente);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent siguiente = new Intent(MainActivity.this, Listar.class);
+                startActivity(siguiente);
+            }
+        });
+
+    }
+
+
+    }
+}
+
+
